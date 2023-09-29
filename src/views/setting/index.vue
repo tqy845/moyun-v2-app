bcad5b6313393834b8b6b8188054c049f2bf1a40
谭期元 <tqy@aigs.ltd>
2023-09-30 02:42:46 +0800

    新增部分设置项（待完善）。

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -11,130 +11,117 @@ import { reactive } from 'vue'
 const cs = reactive({
   tab: 'option-1'
 })
+
+const form = reactive({
+  powerOn: '', // 开机自启
+  autoUpdate: '' // 自动升级
+})
+
+const handleSave = () => {
+  console.log('保存')
+}
 </script>
 
 <template>
   <v-card>
-    <v-card-title class="bg-red d-flex">
-      <v-text-field label="搜索设置" variant="outlined"></v-text-field>
-    </v-card-title>
-    <div class="d-flex flex-row">
-      <v-tabs v-model="cs.tab" direction="vertical" color="primary">
-        <v-tab value="option-1">
-          <v-icon start> mdi-cog </v-icon>
-          基本
-        </v-tab>
-        <v-tab value="option-2">
-          <v-icon start> mdi-send </v-icon>
-          传输
-        </v-tab>
-        <v-tab value="option-3">
-          <v-icon start> mdi-keyboard-settings </v-icon>
-          快捷键
-        </v-tab>
-        <v-tab value="option-4">
-          <v-icon start> mdi-sync </v-icon>
-          同步盘
-        </v-tab>
-      </v-tabs>
-      <v-window v-model="cs.tab">
-        <v-window-item value="option-1">
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam
-                feugiat lorem non metus. Sed a libero.
-              </p>
+    <v-card-item class="">
+      <v-text-field class="py-2" label="搜索设置" variant="outlined"></v-text-field>
 
-              <p>
-                Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus
-                metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam
-                lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
-              </p>
+      <div class="d-flex flex-row">
+        <v-tabs v-model="cs.tab" direction="vertical" color="primary">
+          <v-tab value="option-1">
+            <v-icon start> mdi-cog </v-icon>
+            基本
+          </v-tab>
+          <v-tab value="option-2">
+            <v-icon start> mdi-send </v-icon>
+            传输
+          </v-tab>
+          <v-tab value="option-3">
+            <v-icon start> mdi-keyboard-settings </v-icon>
+            快捷键
+          </v-tab>
+          <v-tab value="option-4">
+            <v-icon start> mdi-sync </v-icon>
+            同步盘
+          </v-tab>
+        </v-tabs>
+        <v-window v-model="cs.tab" class="w-100 pa-5">
+          <v-window-item value="option-1">
+            <v-card flat>
+              <v-card-item>
+                <form fast-fail @submit.prevent="handleSave">
+                  <v-row>
+                    <v-checkbox v-model="form.powerOn" label="开机自动启动（推荐）"></v-checkbox>
+                  </v-row>
+                  <v-row>
+                    <v-checkbox v-model="form.autoUpdate" label="自动升级"></v-checkbox>
+                  </v-row>
+                </form> </v-card-item
+            ></v-card>
+          </v-window-item>
+          <v-window-item value="option-2" class="w-100 pa-5">
+            <v-form>
+              <v-row>
+                <v-col cols="18">
+                  <v-select
+                    class="ma-5"
+                    label="上传并行任务数"
+                    :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+                    variant="outlined"
+                  ></v-select>
+                </v-col>
 
-              <p class="mb-0">
-                Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero
-                tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut
-                odio.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-        <v-window-item value="option-2">
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante
-                convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices
-                tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur
-                ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed
-                turpis.
-              </p>
+                <v-col cols="18">
+                  <v-select
+                    class="ma-5"
+                    label="下载并行任务数"
+                    :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+                    variant="outlined"
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-window-item>
+          <v-window-item value="option-3">
+            <v-card flat>
+              <v-card-text>
+                <p>
+                  Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui.
+                  Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a
+                  accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor
+                  auctor, justo.
+                </p>
 
-              <p>
-                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante
-                convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet
-                ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
-                hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
-              </p>
+                <p class="mb-0">
+                  Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien
+                  ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec,
+                  pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+          <v-window-item value="option-4">
+            <v-card flat>
+              <v-card-text>
+                <p>
+                  Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui.
+                  Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a
+                  accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor
+                  auctor, justo.
+                </p>
 
-              <p>
-                Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida
-                magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed
-                cursus turpis vitae tortor. Aliquam eu nunc.
-              </p>
-
-              <p>
-                Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean
-                viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
-              </p>
-
-              <p class="mb-0">
-                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere
-                eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse
-                enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-        <v-window-item value="option-3">
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin
-                viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan
-                nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor,
-                justo.
-              </p>
-
-              <p class="mb-0">
-                Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien
-                ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec,
-                pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-        <v-window-item value="option-4">
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin
-                viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan
-                nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor,
-                justo.
-              </p>
-
-              <p class="mb-0">
-                Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien
-                ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec,
-                pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-      </v-window>
-    </div>
+                <p class="mb-0">
+                  Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien
+                  ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec,
+                  pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-window-item>
+        </v-window>
+      </div>
+    </v-card-item>
   </v-card>
 </template>
 
