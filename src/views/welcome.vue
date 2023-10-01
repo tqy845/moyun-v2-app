f8635f487c47b136cea30c2a7ed735d6ad3b30d0
谭期元 <tqy@aigs.ltd>
2023-10-01 15:33:39 +0800

    修复和优化多项内容。

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -14,26 +14,44 @@ onMounted(() => {})
 </script>
 
 <template>
-  <v-parallax height="100%" src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
-    <v-row justify="center" align="center" class="flex-column h-50 text-black text-center">
-      <div class="text-h4 font-weight-bold mb-4">摸云 Safe and sound - Mo yun</div>
-      <v-btn variant="text" class="text-none align-center d-flex">
+  <v-parallax
+    height="100%"
+    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    class="h-100"
+  >
+    <v-row
+      style="backdrop-filter: blur(50px); position: relative; top: 11px"
+      justify="center"
+      align="center"
+      class="flex-column h-100 text-grey-lighten-4 text-center"
+    >
+      <div class="text-h4 font-weight-bold mb-4 font-italic" style="text-shadow: 1px 1px 1px black">
+        摸云 Safe and sound - Mo yun
+      </div>
+      <v-btn
+        variant="text"
+        class="text-none align-center d-flex"
+        style="text-shadow: 1px 1px 1px black"
+      >
         欢迎您的使用，当前版本： v2.x Aipha
       </v-btn>
-    </v-row>
-    <v-row justify="center" align="center" class="h-25 text-black text-center">
-      <v-list lines="two" bg-color="transparent">
+      <v-list
+        lines="two"
+        bg-color="transparent"
+        class="mt-16"
+        style="text-shadow: 1px 2px 3px rgb(4, 4, 4)"
+      >
         <v-list-item
-          title="为什么选择 Moyun"
+          title="为什么选择 Moyun ？"
           subtitle="Moyun 是一个功能强大的网盘工具，基于Rust从底层开始构建。我们的UI在应用程序中保持一致的风格， 并提供足够的自定义选项以满足任何使用情况。"
         ></v-list-item>
         <v-list-item
           title="Rust + Tauri"
-          subtitle="使用Rust和Tauri构建快速、安全、可跨平台的桌面应用程序，实现前端与后端分离。"
+          subtitle="使用Rust和Tauri构建快速、安全、跨平台的桌面应用程序，实现前端与后端分离。"
         ></v-list-item>
         <v-list-item
           title="Vue3 + TypeScript + Sass"
-          subtitle="利用Vue 3、TypeScript和Sass创建现代、类型安全、样式可维护的前端Web应用。"
+          subtitle="利用Vue 3、TypeScript和Sass创建现代、类型安全、样式可维护的前端应用。"
         ></v-list-item>
         <v-list-item
           title="SpringBoot + Redis + Mysql"
