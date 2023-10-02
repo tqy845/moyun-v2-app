4e8bf1173cba000e1ec23614e89f9391fca3a61c
谭期元 <tqy@aigs.ltd>
2023-10-02 19:43:21 +0800

    切换语言提示。

--- src/components/common/AppLanguage.vue
+++ src/components/common/AppLanguage.vue
@@ -1,18 +1,39 @@
 <script lang="ts" setup>
-import {languages, type LanguageItem} from '@/plugins/i18n/helper'
-import {useI18n} from 'vue-i18n'
-import {useCookies} from '@vueuse/integrations/useCookies'
+import { languages, type LanguageItem } from '@/plugins/i18n/helper'
+import { useI18n } from 'vue-i18n'
+import { useCookies } from '@vueuse/integrations/useCookies'
+import { reactive } from 'vue'
+import { useUserStore } from '@/stores'
+import { useRouter } from 'vue-router'
 
-const {locale} = useI18n()
+const router = useRouter()
+const userStore = useUserStore()
+const { locale } = useI18n()
 const cookies = useCookies(['locale'])
 /**
  *  切换语言
  * @param item 语言选项
  */
 const handleSwitchLanguage = (item: LanguageItem) => {
-  console.log('切换语言',item)
+  console.log('切换语言', item)
   locale.value = item.value
   cookies.set('locale', item.value)
+  cs.hint.show = true
+}
+
+const cs = reactive({
+  hint: {
+    show: false
+  }
+})
+
+/**
+ * 退出
+ */
+const handleLogout = () => {
+  userStore.logout()
+  router.replace('/login')
+  cs.hint.show = false
 }
 </script>
 
@@ -26,15 +47,29 @@ const handleSwitchLanguage = (item: LanguageItem) => {
     <v-list density="compact" nav>
       <v-list-subheader>{{ $t('toggle.language') }}</v-list-subheader>
       <v-list-item
-          v-for="(item, index) in languages"
-          :key="index"
-          :value="index"
-          @click="handleSwitchLanguage(item)"
+        v-for="(item, index) in languages"
+        :key="index"
+        :value="index"
+        @click="handleSwitchLanguage(item)"
       >
         <v-list-item-title>{{ item.label }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
+
+  <v-dialog width="500" v-model="cs.hint.show">
+    <template v-slot:default="{ isActive }">
+      <v-card :title="$t('change.i18n.hint.title')">
+        <v-card-text> {{ $t('change.i18n.hint.content') }} </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn :text="$t('logout.text')" color="error" @click="handleLogout"></v-btn>
+          <v-btn :text="$t('confirm.text')" @click="isActive.value = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
 </template>
 
 <style lang="scss" scoped></style>
