79d58c77ce47d0b61ef1341477edbfe21c601db4
谭期元 <tqy@aigs.ltd>
2023-10-14 17:12:23 +0800

    temp.

--- src/layout/default-layout/components/AppMenu.vue
+++ src/layout/default-layout/components/AppMenu.vue
@@ -5,7 +5,7 @@
   @description “菜单列表”组件
 -->
 <script lang="ts" setup>
-import {useAppStore, useFileStore} from '@/stores'
+import { useAppStore, useFileStore } from '@/stores'
 
 const appStore = useAppStore()
 const fileStore = useFileStore()
@@ -19,11 +19,11 @@ const handleChangeSecondMenuItem = (item: any) => {
   <v-list :lines="false" nav @click:select="handleChangeSecondMenuItem">
     <!-- 二级菜单 -->
     <v-list-item
-        v-for="(item, index) in fileStore.menuItems"
-        :key="index"
-        :value="item"
-        color="primary"
-        :to="item.path"
+      v-for="(item, index) in fileStore.menuItems"
+      :key="index"
+      :value="item"
+      color="primary"
+      :to="item.path"
     >
       <template v-slot:prepend>
         <v-icon :icon="`mdi-` + item.icon"></v-icon>
